9d1786e4decb10bda7a749fceb0640f386f78305
= <=>
2021-02-02 23:55:55 +0100

    primera subida

--- webEstatica/contacto.php
+++ webEstatica/contacto.php
@@ -1,33 +1,20 @@
 <?php
- 
-if($_POST) {
-    $name = "";
-    $email = "";
-    $message = "";
-     
-    if(isset($_POST['name'])) {
-      $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-    }
-     
-    if(isset($_POST['email'])) {
-        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
-        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-    }
-     
-     
-     
-    $headers  = 'MIME-Version: 1.0' . "\r\n"
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: ' . $visitor_email . "\r\n";
-     
-    if(mail($recipient,$message, $headers)) {
-        echo "<p>Thank you for contacting us, $name. You will get a reply within 24 hours.</p>";
-    } else {
-        echo '<p>We are sorry but the email did not go through.</p>';
-    }
-     
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+$para = 'cdanielgg@hotmail.com';
+$titulo = 'ASUNTO DEL MENSAJE';
+$header = 'From: ' . $email;
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
+  
+if ($_POST['submit']) {
+if (mail($para, $titulo, $msjCorreo, $header)) {
+echo "<script language='javascript'>
+alert('Mensaje enviado, muchas gracias.');
+window.location.href = 'http://web4u.es';
+</script>";
 } else {
-    echo '<p>Something went wrong</p>';
+echo 'Falló el envio';
 }
- 
+}
 ?>
